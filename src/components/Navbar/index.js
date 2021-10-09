6aaa201dc340959d0b885888cbfdf899a2a6939a
tsmith90 <tsmith90@my.westga.edu>
2021-10-09 18:20:23 +0800

    added id information to index.html

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <>
             <Nav>
                 <NavLink to="/">
-                    <h1>Logo</h1>
+                    <h1>Home</h1>
                 </NavLink>
                 <Bars />
                 <NavMenu>
