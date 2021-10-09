35004fdbd687c29a5c48b09248ba0f27ad952132
tsmith90 <tsmith90@my.westga.edu>
2021-10-09 17:28:38 +0800

    added NavBarElements

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import {Nav, NavLink, Bars, NavMenu} from './NavBarElements'
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Nav</h1>
-        </div>
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <h1>Logo</h1>
+                </NavLink>
+                <Bars />
+                <NavMenu>
+                    <NavLink to="/about" activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink to="/blog" activeStyle>
+                        Blog
+                    </NavLink>
+                    <NavLink to="/leetcode" activeStyle>
+                        Leetcode
+                    </NavLink>
+                    <NavLink to="/contact" activeStyle>
+                        Contact
+                    </NavLink>
+                </NavMenu>
+            </Nav>
+        </>
     )
 }
 
