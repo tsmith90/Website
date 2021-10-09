00c453b557ab9bd378fc32687b8a0130a832ef4d
tsmith90 <tsmith90@my.westga.edu>
2021-10-09 17:43:15 +0800

    added template pages for each link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
+import Home from './pages';
+import About from './pages/about';
+import Blog from './pages/blog';
+import Leetcode from './pages/leetcode';
+import Contact from './pages/contact';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/leetcode" component={Leetcode} />
+        <Route path="/Contact" component={Contact} />
+      </Switch>
     </ Router>
   );
 }
---
+++ src/pages/leetcode.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Leetcode = () => {
+    return (
+        <div>
+            <h1>Leetcode</h1>
+        </div>
+    )
+}
+
+export default Leetcode
